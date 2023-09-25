3f62414b04039ec87a963123fe4b8f994f202b50
Marcio Diaz <marcio.diaz@gmail.com>
2023-09-25 17:43:03 +0100

    Final proposal

--- get_neuron_id.sh
+++ get_neuron_id.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export DEVELOPER_NEURON_ID="$(dfx canister --network ic call rceqh-cqaaa-aaaaq-aabqa-cai list_neurons "(record {of_principal = opt principal\"etx6z-5ba6i-vyq4i-6dh2a-deutb-wfxke-lihci-jqyqx-pjs5i-mbs7b-nae\"; limit = 5})" --candid ./governance.did | idl2json  jq -r ".neurons[0].id[0].id" | python3 -c "import sys; ints=sys.stdin.readlines(); sys.stdout.write(bytearray(eval(''.join(ints))).hex())")"
+export DEVELOPER_NEURON_ID="$(dfx canister --network ic call rceqh-cqaaa-aaaaq-aabqa-cai list_neurons "(record {of_principal = opt principal\"hkatf-wjdd6-zgigt-awrjy-wocwv-uqzjn-mvhnz-giwui-2lzag-xhyhs-zae\"; limit = 5})" --candid ./governance.did | idl2json | jq -r ".neurons[0].id[0].id" | python3 -c "import sys; ints=sys.stdin.readlines(); sys.stdout.write(bytearray(eval(''.join(ints))).hex())")"
 
 echo $DEVELOPER_NEURON_ID
